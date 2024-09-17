3f30ffa60bc239b66dbbb7c66c7ba11376d9c158
alexanderdickie <75994927+AlexanderDickie@users.noreply.github.com>
2024-09-17 22:27:11 +0100

    ResultE to utils

--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -1,9 +1,12 @@
-use chrono::{NaiveTime, Duration};
+use chrono::{Duration, NaiveTime};
+use std::error::Error;
 
 pub fn duration_between(start: NaiveTime, end: NaiveTime) -> Duration {
     let dif = end - start;
     match start < end {
         true => dif,
-        false => Duration::hours(24) + dif, 
+        false => Duration::hours(24) + dif,
     }
-} 
+}
+
+pub type ResultE<T> = std::result::Result<T, Box<dyn Error>>;
