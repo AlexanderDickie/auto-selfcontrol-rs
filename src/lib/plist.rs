99dbf9aa09a9387289d4cfffd59c70dab304ed15
alexanderdickie <75994927+AlexanderDickie@users.noreply.github.com>
2024-09-17 22:27:06 +0100

    formatting

--- src/lib/plist.rs
+++ src/lib/plist.rs
@@ -1,13 +1,12 @@
-use chrono::{self, Timelike, NaiveTime, Duration};
+use chrono::{self, Duration, NaiveTime, Timelike};
 
 pub fn build_launch_agent_plist(
     name: &str,
     command: &str,
     args: &Vec<&str>,
     schedule: &LaunchAgentSchedule,
-    run_at_load: bool)
-    -> String 
-{
+    run_at_load: bool,
+) -> String {
     let parts = vec![
         build_plist_header(name),
         build_plist_commands(command, args),
@@ -19,58 +18,63 @@ pub fn build_launch_agent_plist(
 
 fn build_plist_header(name: &str) -> String {
     format!(
-r#"<?xml version="1.0" encoding="UTF-8"?>
+        r#"<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
     <key>Label</key>
-    <string>{}</string>"#
-, name)
+    <string>{}</string>"#,
+        name
+    )
 }
 
 fn build_plist_commands(command: &str, args: &Vec<&str>) -> String {
     let args = args
         .iter()
-        .map(|arg| format!(
-r#"        <string>{}</string>"#
-            , arg))
+        .map(|arg| format!(r#"        <string>{}</string>"#, arg))
         .collect::<Vec<_>>()
         .join("\n");
 
     format!(
-r#"    <key>ProgramArguments</key>
+        r#"    <key>ProgramArguments</key>
     <array>
         <string>{}</string>
 {}
-    </array>"#
-,command, args)
+    </array>"#,
+        command, args
+    )
 }
 
 #[allow(dead_code)]
 pub enum LaunchAgentSchedule<'a> {
-    Calendar(&'a Vec<NaiveTime>), 
-    Periodic(Duration), 
+    Calendar(&'a Vec<NaiveTime>),
+    Periodic(Duration),
 }
 fn build_plist_schedule(schedule: &LaunchAgentSchedule, run_at_load: bool) -> String {
     let timings = match schedule {
         LaunchAgentSchedule::Periodic(duration) => {
-        format!(
-r#"    <key>StartInterval</key>
-        <integer>{}<\integer>"#
-        ,duration.num_seconds())
-        },
+            format!(
+                r#"    <key>StartInterval</key>
+        <integer>{}</integer>"#,
+                duration.num_seconds()
+            )
+        }
 
         LaunchAgentSchedule::Calendar(start_times) => {
             let start_times = start_times
                 .iter()
-                .map(|time| format!(
-r#"       <dict>
+                .map(|time| {
+                    format!(
+                        r#"       <dict>
             <key>Minute</key>
             <integer>{}</integer>
             <key>Hour</key>
             <integer>{}</integer>
-        </dict>"#, 
-                time.minute(), time.hour()))
+        </dict>"#,
+                        time.minute(),
+                        time.hour()
+                    )
+                })
                 .collect::<Vec<_>>()
                 .join("\n");
 
@@ -79,23 +83,25 @@ r#"       <dict>
                 "   <array>".to_string(),
                 start_times,
                 "   </array>".to_string(),
-            ].join("\n")
+            ]
+            .join("\n")
         }
     };
-    // if agent is scheduled to execute when computer is shut down, it will not when next online- run at load 
+    // if agent is scheduled to execute when computer is shut down, it will not when next online- run at load
     // to mitigate this
     if run_at_load {
-        return timings +
-&r#"
+        return timings
+            + &r#"
     <key>RunAtLoad</key>
-    <true/>"#.to_string();
+    <true/>"#
+                .to_string();
     } else {
         return timings;
     }
 }
 
 fn build_plist_footer() -> String {
-r#"</dict>
-</plist>"#.to_string()
+    r#"</dict>
+</plist>"#
+        .to_string()
 }
-
